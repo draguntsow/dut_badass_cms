20df063dafde111b8f03ddb12a02659aa7d37099
draguntsow <ranger_roman@mail.ru>
2019-11-05 12:40:41 +0200

    Create cms_protected_page.php

---
+++ cms_protected_page.php
@@ -0,0 +1,47 @@
+<?php
+#render this page in the way you want to show it to the authorized client - the context depends on your needs.
+if (isset($authorized))
+{
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="icon" href="Favicon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <title>Admin area</title>
+</head>
+<body>
+<div class="card text-center">
+  <div class="card-header">
+    You have successfully logged in
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">ENTER_THE_HEADER</h5>
+    <p class="card-text">ENTER_THE_TEXT_YOU_WANT_TO_SHOW</p>
+  </div>
+  <div class="card-footer text-muted">
+    Message
+  </div>
+</div>
+</body>
+</html>
+<?php
+}
+?>
